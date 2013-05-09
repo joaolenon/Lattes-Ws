8367ed21a8b807fd57f4317b8423b3e6fc4cbb5c
Pedro Fornaza <pedro.fornaza@gmail.com>
2013-05-08 21:59:33 -0300

    Adding routes configuration

--- src/Schoolnet/Application.php
+++ src/Schoolnet/Application.php
@@ -2,7 +2,24 @@
 
 namespace Schoolnet;
 
+use Silex\Application SilexApp;
+
 class Application
 {
+	public function init()
+	{
+		$app = new SilexApp();
 
+		$this->configRoutes($app);
+	}
+
+	public function configRoutes($app)
+	{
+		$configFile = file_get_contents(ROOT_PATH.DS.'config'.DS.'routes.json');
+		$routes = json_decode($configFile);
+
+		foreach ($routes as $route) {
+			$app->get($route->path, $route->target);
+		}
+	}
 }
