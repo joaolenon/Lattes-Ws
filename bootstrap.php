41b042b2d391f2dbe7353175fb466b86dd3a6ec1
João Lenon <jlenonweb@gmail.com>
2013-11-26 02:53:53 -0200

    Fix charset error mysql

--- bootstrap.php
+++ bootstrap.php
@@ -7,5 +7,6 @@ $dbUser     = getenv('LATTES_DB_USER') ?: 'root';
 $dbPass     = getenv('LATTES_DB_PASS') ?: '';
 $dbName     = getenv('LATTES_DB_NAME') ?: 'lattes';
 
-$pdo = new \Pdo("mysql:host={$dbHost};dbname={$dbName}", $dbUser, $dbPass);
+$pdo = new \Pdo("mysql:host={$dbHost};dbname={$dbName}", $dbUser, $dbPass, 
+    array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
